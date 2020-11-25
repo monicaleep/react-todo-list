02e5c29df1414f5d08933faa8c381443ce6bf71a
Monica <monica.parrillo@gmail.com>
2020-11-24 20:20:05 -0600

    Add 'add new item' feature. Completed deliverable

--- src/MyList.js
+++ src/MyList.js
@@ -1,31 +1,49 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import ListItem from './ListItem'
 
 class MyList extends Component {
   state = {
-    toDoItemArray : this.props.theList
+    toDoItemArray: this.props.theList,
+    newItem: ''
   }
-  clearList = (e) =>{
+  clearList = (e) => {
+    this.setState({toDoItemArray: []})
+  }
+  newItemChange = (e) => {
+    this.setState({newItem: e.target.value})
+  }
+  addItem = (e) => {
+    e.preventDefault()
     this.setState({
-      toDoItemArray: []
+      toDoItemArray: [
+        ...this.state.toDoItemArray,
+        this.state.newItem
+      ],
+      newItem: ""
     })
   }
   render() {
-    const listItems = this.state.toDoItemArray.map((item,idx)=>{
-      return <ListItem doThis={item} key={idx} />
+    const listItems = this.state.toDoItemArray.map((item, idx) => {
+      return <ListItem doThis={item} key={idx}/>
     })
-    return (
-      <div>
-        <h1> Things I should stop procrastinating:</h1>
-        <ul>
-          {listItems}
-        </ul>
-        <button onClick={(e)=>this.clearList(e)}>Finished the List!</button>
-      </div>
-    )
+    return (<div>
+      <h1>
+        Things I should stop procrastinating:</h1>
+      <ul>
+        {listItems}
+      </ul>
+      <button onClick={(e) => this.clearList(e)}>Finished the List!</button>
+      <form>
+        <input
+          type="text"
+          placeholder="Type an item here"
+          onChange={(e) => this.newItemChange(e)}
+          value={this.state.newItem}/>
+        <button onClick={(e) => this.addItem(e)}>Add it!</button>
+      </form>
+    </div>)
   }
 }
-
 
 export default MyList;
